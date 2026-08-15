f63b7ea8e526a9c1c465cfb8b68e7fe961470011
maxhuntorlean-debug <maxhuntorlean@gmail.com>
2026-08-15 21:53:38 +0200

    Move navigation from app into router

--- public/js/app.js
+++ public/js/app.js
@@ -1,34 +1,20 @@
 import { authMe } from "./api.js";
 import { setCurrentUser } from "./state.js";
-import { showLogin } from "./pages/login.js";
-import { showHome } from "./pages/home.js";
-import { showSale } from "./pages/sale.js";
-import { showIncome } from "./pages/income.js";
-import { showJournal } from "./pages/journal.js";
-import { showReport } from "./pages/report.js";
-import { showLog } from "./pages/log.js";
+import { initRouter, navigate } from "./router.js";
 
 const app = document.getElementById("app");
-
-const routes = {};
-routes.login = () => showLogin(app, routes.home);
-routes.home = () => showHome(app, routes);
-routes.sale = () => showSale(app, routes.home);
-routes.income = () => showIncome(app, routes.home);
-routes.journal = () => showJournal(app, routes.home);
-routes.report = () => showReport(app, routes.home);
-routes.log = () => showLog(app, routes.home);
+initRouter(app);
 
 async function start(){
   try{
     const me = await authMe();
     if(me.success){
       setCurrentUser(me.data);
-      routes.home();
+      navigate("home");
       return;
     }
   }catch{}
-  routes.login();
+  navigate("login");
 }
 
 start();
